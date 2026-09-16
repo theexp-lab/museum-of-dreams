270fba2f5a7b5da82fecd7c458364f4398690410
the experience laboratory <louann.desia@gmail.com>
2026-09-16 15:01:06 +0200

    Update romance.js

--- romance.js
+++ romance.js
@@ -421,6 +421,26 @@ function updateStoryLines() {
     }
   );
 
+     if (
+    strongestLine &&
+    strongestLine !== activeLine
+  ) {
+    activeLine =
+      strongestLine;
+
+
+    dreamTitle.textContent =
+      strongestLine.dataset.title;
+
+
+    emotionalCore.textContent =
+      strongestLine.dataset.emotion;
+
+
+    dreamMotif.textContent =
+      strongestLine.dataset.motif;
+  }
+}
 
 /* ==========================================
    AUDIO
